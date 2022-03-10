eb9d58a3839e5fe05c1eae68a87e8fc49f3aea8f
Eduardo N Gomes <Eduardo.000@live.com>
2022-03-10 16:42:49 -0300

    Exercicio 86 Mundo 3

---
+++ Exercicios/pythonProject/ex086.py
@@ -0,0 +1,32 @@
+#Exercício Python 086: Crie um programa que declare uma matriz de dimensão 3x3 e preencha com valores lidos pelo teclado. No final, mostre a matriz na tela, com a formatação correta.
+
+#numeros = [[],[],[],[],[],[],[],[],[]]
+
+#for c in range(0,9):
+#    n = int(input('Valores: '))
+#    numeros[c].append(n)
+#
+#for c in range(0,3):
+#    print(numeros[c], end=' ')
+#print()
+#for c in range(3,6):
+#    print(numeros[c], end=' ')
+#print()
+#for c in range(6,9):
+#    print(numeros[c], end=' ')
+
+
+#           CORRECAO DO PROFESSOR
+
+matriz = [[0,0,0],[0,0,0],[0,0,0]]
+
+for l in range(0,3):
+    for c in range(0,3):
+        matriz[l][c] = int(input(f'Digite um valor para [{l}, {c}]: '))
+
+print('-='*30)
+
+for l in range(0,3):
+    for c in range(0,3):
+        print(f'[{matriz[l][c]}]',end='')
+    print()
