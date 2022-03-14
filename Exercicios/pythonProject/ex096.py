99fdfbaf9d3f08e8d82d755870d6e37840856141
Eduardo N Gomes <Eduardo.000@live.com>
2022-03-14 16:52:09 -0300

    Exercicio 98 Mundo 3

--- Exercicios/pythonProject/ex096.py
+++ Exercicios/pythonProject/ex096.py
@@ -1,15 +1,15 @@
 #Exercício Python 096: Faça um programa que tenha uma função chamada área(), que receba as dimensões de um terreno retangular (largura e comprimento) e mostre a área do terreno.
 
-def area(a,b):
-    t = a*b
-    print(f'A area de um terreno de {a}x{b} e de {t}m².')
+def calcArea(a,b):                                          #Criacao da funcao area
+    t = a*b                                                 #Realizando o calcula da area   
+    print(f'A area de um terreno de {a}x{b} e de {t}m².')   #Mostrando parametros passados com o resultado 
 
 
 #Programa principal
-print('     Controle de terrenos     ')
-print('-'*10)
+print('     Controle de terrenos     ')                     #APENAS FORMATACAO
+print('-'*10)                                               #APENAS FORMATACAO
 
-a = float(input('Largura (m): '))
-b = float(input('Coprimento (m): '))
+a = float(input('Largura (m): '))                           #Armazenando um parametro
+b = float(input('Coprimento (m): '))                        #Armazenando um parametro
 
-area(a, b)
+calcArea(a, b)                                              #Chamando a funcao calcArea passando os parametros
--- Exercicios/pythonProject/ex097.py
+++ Exercicios/pythonProject/ex097.py
@@ -1,10 +1,10 @@
 #Exercício Python 097: Faça um programa que tenha uma função chamada escreva(), que receba um texto qualquer como parâmetro e mostre uma mensagem com tamanho adaptável.
 
-def escreva(txt):
-    print('-'*(len(txt)+4))
-    print(f'  {txt}')
-    print('-'*(len(txt)+4))
+def escreva(txt):                   #Criando a funcao escreva
+    print('-'*(len(txt)+4))         #Apenas formatacao de acordo com a qdt de letras que o parametro possui
+    print(f'  {txt}')               #Exibindo o parametro
+    print('-'*(len(txt)+4))         #Apenas formatacao de acordo com a qdt de letras que o parametro possui
 
 
-escreva('Ola, mundo')
-escreva('Oi')
+escreva('Ola, mundo')               #Chamando funcao passando o parametro
+escreva('Oi')                       #Chamando funcao passando o parametro
