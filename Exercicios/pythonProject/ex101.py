4a5f8c7df85cd18321cbd368dd8c7f746487e27b
Eduardo N Gomes <Eduardo.000@live.com>
2022-03-15 15:49:55 -0300

    Exercicio 102 mundo 3

--- Exercicios/pythonProject/ex101.py
+++ Exercicios/pythonProject/ex101.py
@@ -2,19 +2,19 @@
 
 
 
-def voto(dob):
-    from datetime import datetime
+def voto(dob):                                                  #Criacao da funcao voto passando por parametro ano do nascimento(dob).
+    from datetime import datetime                               #Importando biblioteca para sabe ano atual.
 
-    age = datetime.today().year - dob
+    age = datetime.today().year - dob                           #Calculando e armazenando idade.
     
-    if age >= 65:
-        return f'Voec possui {age} anos. VOTO OPCIONAL'
-    elif age >= 18:
-        return f'Voce possui {age} anos. VOTO OBRIGATORIO'
-    elif age >= 16:
-        return f'Voce possui {age} anos. VOTO OPCIONAL'
-    else:
-        return f'Voce possui {age} anos. VOTO NEGADO'
+    if age >= 65:                                               #Verificando a idade
+        return f'Voec possui {age} anos. VOTO OPCIONAL'         #Retornando resposta de acordo com a idade
+    elif age >= 18:                                             #Verificando a idade
+        return f'Voce possui {age} anos. VOTO OBRIGATORIO'      #Retornando resposta de acordo com a idade
+    elif age >= 16:                                             #Verificando a idade
+        return f'Voce possui {age} anos. VOTO OPCIONAL'         #Retornando resposta de acordo com a idade
+    else:                                                       #Verificando a idade
+        return f'Voce possui {age} anos. VOTO NEGADO'           #Retornando resposta de acordo com a idade
 
-n = int(input('Qual seu ano de nascimento? '))
-print(voto(n))
+n = int(input('Qual seu ano de nascimento? '))                  #Armazenado ano do nascimento
+print(voto(n))                                                  #chamando a funcao voto passando ano do nascimento como parametro
---
+++ Exercicios/pythonProject/ex102.py
@@ -0,0 +1,21 @@
+# Exercício Python 102: Crie um programa que tenha uma função fatorial() que receba dois parâmetros: o primeiro que indique o número a calcular e outro chamado show, que será um valor lógico (opcional) indicando se será mostrado ou não na tela o processo de cálculo do fatorial.
+
+def fatorial(n, show=False):                     #Criando funcao fatorial
+    f = 1                                        #Variavel de controle
+    if show == True:                             #Verificando se a funcao tem o parametro show == True
+        for c in range(n, 0, -1):                #Loop para calcular o fatorial   
+            f *= c                               #Varivel de controle  fazendo o calculo necessario 
+            if c == 1:                           #Quando o C for igual a 1 ira mostrar '=' apenas para FORMATACAO  
+                print('1 ',end='= ')
+            else:
+                print(f'{c}',end= ' x ')        #Mostrando os valores de C para FORMATACAO  
+        return f                                #Retornando o resultado
+    else:
+        for c in range(n, 0, -1):               #Loop para calcular o fatorial  
+            f *= c                              #Varivel de controle  fazendo o calculo necessario 
+        return f                                #Retornando o resultado
+
+
+
+print(fatorial(5, True))                        #Chamando funcao fatorial passando 5 e True como parametros
+print(fatorial(5))                              #Chamando funcao fatorial passando apenas 5 como parametro 
